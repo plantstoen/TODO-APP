5dc02a5eccf840bfd8dfb363302e204baec71fdc
plantstoen <plantstoen@gmail.com>
2020-01-06 13:01:08 +0900

    component optimization(rerender, list render process

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useRef, useCallback } from 'react';
+import TodoTemplate from './components/TodoTemplate';
+import TodoInsert from './components/TodoInsert';
+import TodoList from './components/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function createBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({ id: i, text: `todo ${i}`, checked: false });
+  }
+
+  return array;
 }
+
+function todoReducer(todos, action) {
+  switch (action.type) {
+    case 'INSERT':
+      return todos.concat(action.todo);
+    case 'REMOVE':
+      return todos.filter(todo => todo.id !== action.id);
+    case 'TOGGLE':
+      return todos.map(todo =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo,
+      );
+    default:
+      return todos;
+  }
+}
+
+const App = () => {
+  // 원래는 두번쨰 파라미터에 초기상태를 넣어주는데, 대신에 지금은 undefined를 넣고 세번째 파라미터에 함수를 넣음
+  // => 맨 처음 렌더링될 때만 createBulkTodos가 호출
+  const [todos, dispatch] = useReducer(todoReducer, undefined, createBulkTodos);
+
+  const nextId = useRef(4);
+
+  const onInsert = useCallback(text => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    dispatch({ type: 'INSERT', todo });
+    nextId.current += 1;
+  }, []);
+
+  const onRemove = useCallback(id => {
+    dispatch({ type: 'REMOVE', id });
+  }, []);
+
+  const onToggle = useCallback(id => {
+    dispatch({ type: 'TOGGLE', id });
+  }, []);
+
+  return (
+    <TodoTemplate>
+      <TodoInsert onInsert={onInsert} />
+      <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
+    </TodoTemplate>
+  );
+};
 
 export default App;
